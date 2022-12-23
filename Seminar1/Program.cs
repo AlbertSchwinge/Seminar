068c0581e62690ffda49484081c605b8c73b31c1
unknown <ebay24@gmail.com>
2022-12-23 15:35:24 +0100

    seminar1 houn1

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -4,7 +4,7 @@
 
 // задача 1 (является ли число 1 квадратом от числа 2)
 /*
-Console.Write("input a frist :");
+Console.Write("input a first :");
 int numer1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("input a sekond :");
 int numer2 = Convert.ToInt32(Console.ReadLine());
@@ -26,5 +26,16 @@ while (carent <= numer )
     Console.Write(carent + " " );
     carent = carent + 1 ; } 
 */
+// Домашнее задание семинар 1 
+// задача первая , напишите программу которая принимает два числа и на выходе выдаёт какое больше а какое меньше
+Console.Write("input a first :") ;
+int N1 = Convert.ToInt32(Console.ReadLine()) ;
+Console.Write("input a sekond :") ;
+int N2 = Convert.ToInt32(Console.ReadLine()) ;
+if (N1<N2)
+   Console.Write((N1)+"<"+(N2));
+else
+   Console.Write((N1)+">"+(N2));
+   
 
 
