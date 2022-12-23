afc0b037351b8684b26a528356392fbaea748102
unknown <ebay24@gmail.com>
2022-12-23 14:05:14 +0100

    Seminar 1 and

---
+++ Seminar1/Program.cs
@@ -0,0 +1,30 @@
+﻿// семинар 1
+
+
+
+// задача 1 (является ли число 1 квадратом от числа 2)
+/*
+Console.Write("input a frist :");
+int numer1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input a sekond :");
+int numer2 = Convert.ToInt32(Console.ReadLine());
+int quad2 = numer2*numer2;
+if (numer1==quad2)
+   Console.Write("Yes!");
+else 
+   Console.Write("Nou");
+*/
+
+// задача2 написать программу которая показывает все целые числа , в промежутке от минус числа numer до числа numer
+
+/*
+Console.Write("input numer : ");
+int numer = Convert.ToInt32(Console.ReadLine());
+int carent = -numer;
+while (carent <= numer )
+    {
+    Console.Write(carent + " " );
+    carent = carent + 1 ; } 
+*/
+
+
